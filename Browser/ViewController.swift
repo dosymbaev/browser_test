75dba2cd2ac0f7ccf7bf904998da79a43fb52846
Dias <diasdosymbaev@gmail.com>
2023-05-29 09:05:01 +0600

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Dias Dossymbayev on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
